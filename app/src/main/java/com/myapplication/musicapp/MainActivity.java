32c71f0bf4b57cd3e05aa8d423afd56e4bc1e4d7
Ronak9595 <ronakgupta9595@gmail.com>
2024-05-26 16:03:40 +0530

    Music App-First Day

---
+++ app/src/main/java/com/myapplication/musicapp/MainActivity.java
@@ -0,0 +1,133 @@
+package com.myapplication.musicapp;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    String[] items;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listViewSong);
+        runtimePermission();
+    }
+
+    public void runtimePermission() {
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        displaySongs();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        // Handle permission denial
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest request, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+    public ArrayList<File> findSong(@NonNull File file) {
+        ArrayList<File> arrayList = new ArrayList<>();
+        File[] files = file.listFiles();
+
+        if (files != null) {
+            for (File singleFile : files) {
+                if (singleFile.isDirectory() && !singleFile.isHidden()) {
+                    arrayList.addAll(findSong(singleFile));
+                } else {
+                    if (singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav")) {
+                        arrayList.add(singleFile);
+                    }
+                }
+            }
+        }
+        return arrayList;
+    }
+
+    void displaySongs() {
+        ArrayList<File> allSongs = new ArrayList<>();
+
+        // Get songs from external storage
+        File externalStorage = Environment.getExternalStorageDirectory();
+        if (externalStorage != null && externalStorage.exists()) {
+            allSongs.addAll(findSong(externalStorage));
+        }
+
+        // Get songs from the internal storage directories where user media files are typically stored
+        File internalStorage = getExternalFilesDir(Environment.DIRECTORY_MUSIC);
+        if (internalStorage != null && internalStorage.exists()) {
+            allSongs.addAll(findSong(internalStorage));
+        }
+
+        items = new String[allSongs.size()];
+
+        for (int i = 0; i < allSongs.size(); i++) {
+            items[i] = allSongs.get(i).getName().replace(".mp3", "").replace(".wav", "");
+        }
+
+        /*ArrayAdapter<String> myAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(myAdapter);*/
+
+        customAdapter customAdapter=new customAdapter();
+        listView.setAdapter(customAdapter);
+    }
+    class customAdapter extends BaseAdapter
+    {
+
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            View myView=getLayoutInflater().inflate(R.layout.list_bg,null);
+            TextView textsong=myView.findViewById(R.id.txtsongname);
+            textsong.setSelected(true);
+            textsong.setText(items[i]);
+
+            return myView;
+
+        }
+    }
+}
